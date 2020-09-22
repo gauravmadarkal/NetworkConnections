e8c40f5a22bbcf296da6fb4f1da61b10d4d08915
gauravmadarkal <madarkal.gaurav@gmail.com>
2020-09-22 15:08:38 +0530

    added sample usage class

---
+++ NetworkConnections/sample-usage/NetworkInformationHelper.cs
@@ -0,0 +1,24 @@
+﻿using NetworkConnections.src.Client.Implementation;
+using NetworkConnections.src.Wlan.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkConnections.sample_usage
+{
+    public class NetworkInformationHelper
+    {
+        public void FetchNetworkInfo()
+        {
+            NetworkInformation networkInformation = new NetworkInformation();
+            ConnectionInfo connectionInfo = networkInformation.ConnectionInfo;
+            string wifiSSID = connectionInfo.WlanInfo.SSID;
+            bool isPasswordProtected = connectionInfo.WlanInfo.IsSecured;
+            NetworkCategory category = connectionInfo.WlanInfo.NetworkCategory;
+            bool isPublicNetwork = category == NetworkCategory.Public;
+            string lanName = connectionInfo.LanInfo.Name;
+        }
+    }
+}
